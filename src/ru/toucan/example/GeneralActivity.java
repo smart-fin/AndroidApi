313af341cfbbe09e21bb62748d7924e1bda6927a
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2014-08-18 00:02:10 +0400

    2014-08-18

--- src/ru/toucan/example/GeneralActivity.java
+++ src/ru/toucan/example/GeneralActivity.java
@@ -1,10 +1,8 @@
 package ru.toucan.example;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +10,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class GeneralActivity extends Activity {
+
+    private static final int PAYMENT_RESULT_CODE = 0x101010;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,14 +38,7 @@ public class GeneralActivity extends Activity {
                     return;
 
                 }
-                makePayment(amountView.getText().toString(), descView.getText().toString());
-            }
-        });
-
-        findViewById(R.id.btnHistory).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showHistoryPayments();
+                makePayment(Double.parseDouble(amountView.getText().toString()), descView.getText().toString());
             }
         });
 
@@ -61,7 +54,7 @@ public class GeneralActivity extends Activity {
      * <p/>
      * Если клиент уже был ранее активирован, будет запрошен пин-код
      */
-    private void makePayment(String amount, String desc) {
+    private void makePayment(double amount, String desc) {
 
         Intent intent = new Intent("ru.toucan.CALL_FOR_PAYMENT");
         // packageName - имя пакета для возвращения результата проведения платежа
@@ -71,31 +64,19 @@ public class GeneralActivity extends Activity {
         intent.putExtra("amount", amount);
         // desc - назначение платежа
         intent.putExtra("desc", desc);
-        startActivity(intent);
+        startActivityForResult(intent, PAYMENT_RESULT_CODE);
+    }
 
-        // Регистрируем ресивер для получения ответа
-        IntentFilter pluginFilter = new IntentFilter();
-        pluginFilter.addAction("ACTION_DELIVER_PAYMENT");
-        registerReceiver(receiver, pluginFilter);
+    public static String getVersionName(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (android.content.pm.PackageManager.NameNotFoundException e) {
+            return " - ";
+        }
     }
 
     /**
-     * Запрос истории операций
-     */
-    private void showHistoryPayments() {
-        Intent intent = new Intent("ru.toucan.CALL_FOR_HISTORY");
-        // packageName - имя пакета для возвращения результата проведения платежа
-        intent.putExtra("packageName", getPackageName());
-        intent.putExtra("pin", "1111");
-        startActivity(intent);
-
-        // Регистрируем ресивер для получения ответа
-        IntentFilter pluginFilter = new IntentFilter();
-        pluginFilter.addAction("ACTION_DELIVER_HISTORY");
-        registerReceiver(receiver, pluginFilter);
-    }
-
-    /**
+     * Ответ
      * Result success = new Result(0, "success");
      * Result success_put_to_queue = new Result(1, "success_put_to_queue");
      * Result server_error = new Result(20, "server_error");
@@ -105,36 +86,31 @@ public class GeneralActivity extends Activity {
      * Result network_is_available = new Result(5, "network_is_available");
      * Result system_service_error = new Result(6, "system_service_error");
      * Result unknown_error = new Result(100, "unknown_error");
+     * Result cancel_operation = new Result(200, "Отмена операции");
+     * Result waiting_timeout = new Result(400, "waiting_timeout");
      * Result reader_not_init = new Result(1001, "Ридер не инициализирован");
      * Result reader_not_identify = new Result(1002, "Неизвестный ридер");
      * Result transaction_aborted_by_card = new Result(1003, "Операция отклонена картой");
      * Result card_blocked = new Result(1004, "Карта заблокирована");
+     * Result dont_use_card = new Result(1005, "Не принимайте эту карту к оплате!");
      * Result insert_card = new Result(13, "Вставьте карту чипом в ридер");
+     * Result try_payment_again = new Result(14, "Попробуйте провести платёж заново");
      * Result payment_not_confirmed = new Result(15, "Платеж не подтвержден");
      * Result payment_canceled = new Result(16, "Платеж отменен");
      */
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context ctxt, Intent i) {
-            Log.d("2can", "got in receiver EXTRA_CONTENT");
-            if ("ACTION_DELIVER_PAYMENT".equals(i.getAction())) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-                int result = i.getIntExtra("result", -1);
-                Log.d("2can", "got in receiver " + result);
-                String msg = result == 0 ? "Платеж завершен успешно" : "Ошибка платежа";
-                msg += "\nСпособ оплаты: " + i.getStringExtra("paymentType");
-                Alert.show("Внимание", msg, "Ок", null, GeneralActivity.this);
-                // после получения ответа - удаляем ресивер
-                unregisterReceiver(receiver);
-            }
+        Log.d("2can", "got result " + resultCode);
+        String msg = null;
+        if (data == null) {
+            msg = "Ошибка. ErrorCode: " + resultCode;
+        } else {
+            msg = resultCode == 0 ? "Платеж завершен успешно" : ("Ошибка платежа. ErrorCode: " + resultCode);
+            msg += "\nСпособ оплаты: " + data.getStringExtra("paymentType");
         }
-    };
-
-    public static String getVersionName(Context context) {
-        try {
-            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
-        } catch (android.content.pm.PackageManager.NameNotFoundException e) {
-            return " - ";
-        }
+        Alert.show("Внимание", msg, "Ок", null, GeneralActivity.this);
+        // после получения ответа - удаляем ресивер
     }
 }
